d5fe92aab317c2154dc2a5c95754e9aa58fe30a2
zhoulifu <zlf369777684@gmail.com>
2017-03-31 17:53:59 +0800

    Follow the guide of creating archetype

---
+++ src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+package ${package};
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
